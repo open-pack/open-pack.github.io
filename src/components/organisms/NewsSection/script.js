8452a755ddb56aeb6ba81d678032442554433dea
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-03-22 01:10:45 +0900

    feat: update news section

--- src/components/organisms/NewsSection/script.js
+++ src/components/organisms/NewsSection/script.js
@@ -2,11 +2,17 @@ export default {
   name: 'NewsSection',
   data: () => ({
       articles: [
-          {
+        {
               color: 'grey lighten-1',
               icon: 'mdi-information',
-              subtitle: 'Jan 05, 2021',
-              title: 'Open this site',
+              subtitle: 'Jan 05, 2022',
+              title: 'Site Open',
+        },
+        {
+          color: 'grey lighten-1',
+          icon: 'mdi-information',
+          subtitle: 'March 22, 2022',
+          title: 'Release - Dataset (v0.1.0) ',
         },
       ],
   }),
