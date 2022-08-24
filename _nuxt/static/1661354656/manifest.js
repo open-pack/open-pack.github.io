0d1b2606b7c4bd93900141fda0f8241cbe8adc04
getty708 <getty708@users.noreply.github.com>
2022-08-24 15:24:21 +0000

    deploy: 28195863c37c267adda1c484037721b02abdee10

---
+++ _nuxt/static/1661354656/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticles\u002F2022\u002Fsample","\u002Frelease\u002Fv0-2-1","\u002Farticles","\u002Frelease","\u002Fchallenge2022","\u002Fgallery"]})
