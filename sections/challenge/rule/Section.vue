84565ce1176d393bf143c358fa2b3907569f7869
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-09-17 02:02:50 +0400

    feat: add rule section to OPC2022 #13

---
+++ sections/challenge/rule/Section.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="gnt-sec-2">
+    <section class="gnt-sec-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h2>Rules</h2>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <h3>A. No Private Sharing Outside Teams</h3>
+            <p>
+              Privately sharing code or data outside of teams is not permitted.
+              It’s OK to share code if made available to all participants on the
+              forum.
+            </p>
+            <h3>B. External Dataset</h3>
+            <p>
+              You may use data other than the competition data to develop and
+              test your submission. However, you will ensure the external data
+              is publicly available to everyone without any cost.
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+  </section>
+</template>
+<script scoped>
+export default {
+  name: 'ChallengeRuleSection',
+}
+</script>
+<style lang="scss" scoped></style>
