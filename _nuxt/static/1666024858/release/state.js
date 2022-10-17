716eefaa282b21f95ae04f561bfe1e1554eeaf0c
getty708 <getty708@users.noreply.github.com>
2022-10-17 16:41:01 +0000

    deploy: a37220c5550f01d6a3d32e23d3e44283b6753fcb

---
+++ _nuxt/static/1666024858/release/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666024858",layout:"default",error:a,serverRendered:true,routePath:"\u002Frelease",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1a3c0f68"}}}}(null));
