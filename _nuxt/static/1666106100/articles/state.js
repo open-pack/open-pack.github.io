46da10405de21dcd1db361ff3427274e5e120deb
getty708 <getty708@users.noreply.github.com>
2022-10-18 15:15:04 +0000

    deploy: 0bfc514e5b9b4b1362b8e216f384b675124137c2

---
+++ _nuxt/static/1666106100/articles/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666106100",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"47926f7c"}}}}(null));
