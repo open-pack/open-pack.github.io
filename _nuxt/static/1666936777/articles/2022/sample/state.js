3ad79eb91574cc5ba67ef32c8c6fe998ad3dae61
getty708 <getty708@users.noreply.github.com>
2022-10-28 05:59:41 +0000

    deploy: 03da589d98545827b21273422676e626317dcc1f

---
+++ _nuxt/static/1666936777/articles/2022/sample/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666936777",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002F2022\u002Fsample",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09c321e7"}}}}(null));
---
+++ _nuxt/static/1666936777/release/v0-2-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666936777",layout:"default",error:a,serverRendered:true,routePath:"\u002Frelease\u002Fv0-2-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09c321e7"}}}}(null));
