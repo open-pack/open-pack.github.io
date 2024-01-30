868f290ea7868ab858079f3f0ce7a1887e29e85a
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-01-31 00:00:04 +0900

    fix: change link order

--- sections/lp/top/DatasetSec.vue
+++ sections/lp/top/DatasetSec.vue
@@ -1,49 +1,15 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="6" class="py-1">
+      <v-col v-for="(link, i) in links" :key="i" cols="12" md="6" class="py-1">
         <v-btn
-          v-scroll-to="`#download`"
-          class="py-1"
-          block
-          color="op-brown darken-1 white--text text-h6"
-        >
-          <span class="text-decoration-none">
-            <span class="text-capitalize">Download</span>
-          </span>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" md="6" class="py-1">
-        <v-btn
-          v-scroll-to="`#dataset-docs-toc`"
+          v-scroll-to="link.tagId"
           class="py-2"
           block
           color="op-brown darken-1 white--text text-h6"
         >
           <span class="text-decoration-none text-capitalize">
-            Dataset Docs
-          </span>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" md="6" class="py-1">
-        <v-btn
-          v-scroll-to="`#tutorials`"
-          class="py-2"
-          block
-          color="op-brown darken-1 white--text text-h6"
-        >
-          <span class="text-decoration-none text-capitalize"> Tutorials </span>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" md="6" class="py-1">
-        <v-btn
-          v-scroll-to="`#papers`"
-          class="py-2"
-          block
-          color="op-brown darken-1 white--text text-h6"
-        >
-          <span class="text-decoration-none text-capitalize">
-            Conference Papers
+            {{ link.title }}
           </span>
         </v-btn>
       </v-col>
@@ -53,6 +19,26 @@
 <script scoped>
 export default {
   name: 'DatasetSec',
+  data: () => ({
+    links: [
+      {
+        title: 'Dataset Docs',
+        tagId: '#dataset-docs-toc',
+      },
+      {
+        title: 'Download',
+        tagId: '#download',
+      },
+      {
+        title: 'Tutorials',
+        tagId: '#tutorials',
+      },
+      {
+        title: 'Conference Papers',
+        tagId: '#papers',
+      },
+    ],
+  }),
 }
 </script>
 <style lang="scss" scoped>
