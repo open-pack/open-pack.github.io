232e7120f0b95557d38933401412a91ebc89691e
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-04-21 12:27:21 +0900

    feat: update citation

--- sections/lp/dataset-overview/Section.vue
+++ sections/lp/dataset-overview/Section.vue
@@ -14,7 +14,20 @@
               devices and rich metadata such as error annotation and order
               information.
             </p>
+            <p>
+              The latest release is
+              <a :href="releaseNote" target="_blank"
+                >OpenPack Dataset v1.0.0. (Release Note)</a
+              >
+            </p>
             <v-img :src="keyVisual"></v-img>
+            <v-sheet color="op-brown lighten-2" rounded class="pa-3">
+              Paper: N. Yoshimura, J. Morales, T. Maekawa and T. Hara,
+              "OpenPack: A Large-Scale Dataset for Recognizing Packaging Works
+              in IoT-Enabled Logistic Environments," 2024 IEEE International
+              Conference on Pervasive Computing and Communications (PerCom),
+              Biarritz, France, 2024 (<a :href="paperLink">link</a>)
+            </v-sheet>
           </v-col>
         </v-row>
       </v-container>
@@ -30,6 +43,9 @@ export default {
   name: 'OverviewSec',
   data: () => ({
     keyVisual: require('@/assets/img/dataset/OpenPack__KeyVisual.png'),
+    releaseNote:
+      'https://github.com/open-pack/openpack-dataset/tree/main/release/v1.0.0',
+    paperLink: 'https://ieeexplore.ieee.org/abstract/document/10494448',
   }),
 }
 </script>
